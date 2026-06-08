2474ccd29e00bc2efaa120c3452ac3f87407243e
Mustafa Gunaltay <mustafa-gunaltay@hotmail.com>
2026-06-08 12:51:46 +0300

    try asset route

---
+++ app/controllers/ForTryController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc.{MessagesAbstractController, MessagesControllerComponents}
+import repositories.TodoRepository
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class ForTryController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+  def try1 = TODO
+
+  def try2 = Action {
+    Ok("Try 2")
+  }
+}
+
